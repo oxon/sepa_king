29a60497b03c4477631756107e7434c700e8662f
Guido Gloor <haslo@haslo.ch>
2017-09-26 15:46:32 +0200

    Continued type detection for schema specific validations

--- lib/sepa_king/transaction/credit_transfer_transaction.rb
+++ lib/sepa_king/transaction/credit_transfer_transaction.rb
@@ -10,26 +10,30 @@ module SEPA
     end
 
     def schema_compatible?(schema_name)
-      if schema_name != PAIN_001_001_03_CH_02
+      # massive cyclomatic complexity, TODO: reduce
+      if schema_name == PAIN_001_001_03_CH_02
+        self.payment_method.in?(%w(TRF TRA)) && ch_payment_type_valid?
+      else
         self.service_level ||= 'SEPA'
-        return false if self.local_instrument.present? || self.payment_method.present? || self.creditor_agent.present? || self.reference_number.present?
-        return false unless self.service_level.in?(%w(SEPA URGP))
-      end
-      case schema_name
-        when PAIN_001_001_03
-          self.service_level == 'SEPA'
-        when PAIN_001_002_03
-          self.bic.present? && self.service_level == 'SEPA' && self.currency == 'EUR'
-        when PAIN_001_003_03
-          self.currency == 'EUR'
-        when PAIN_001_001_03_CH_02
-          return false unless self.payment_method.in?(%w(TRF TRA))
-          return false unless ch_payment_type_valid?
+        !(self.local_instrument.present? || self.payment_method.present? || self.creditor_agent.present? || self.reference_number.present?) &&
+          (self.service_level.in?(%w(SEPA URGP))) &&
+          case schema_name
+            when PAIN_001_001_03
+              self.service_level == 'SEPA'
+            when PAIN_001_002_03
+              self.bic.present? && self.service_level == 'SEPA' && self.currency == 'EUR'
+            when PAIN_001_003_03
+              self.currency == 'EUR'
+          end
       end
     end
 
     def ch_payment_type_valid?
+      # massive cyclomatic complexity, TODO: reduce
       if self.service_level == 'SEPA' # type 5
+        # this is actually almost the international standard
+        !(self.local_instrument.present? || self.payment_method.present? || self.creditor_agent.present? || self.reference_number.present?) &&
+          self.currency == 'EUR'
       else
         case self.local_instrument
           when 'CH01' # type 1
@@ -37,16 +41,13 @@ module SEPA
           when 'CH02' # type 2.1
             self.service_level.nil? && self.currency.in?(%w(CHF EUR)) && self.creditor_agent.nil? && self.reference_number.nil?
           when 'CH03' # type 2.2
-            self.service_level.nil? && self.currency.in?(%w(CHF EUR)) && self.creditor_agent.present? && self.reference_number.nil?
+            self.service_level.nil? && self.currency.in?(%w(CHF EUR)) && self.creditor_agent.present? && self.reference_number.nil? &&
+              self.creditor_agent.is_a?(Hash) && [[:iid], [:account, :iid], [:account, :bank_name]].any? { |keys| self.creditor_agent.keys.sort == keys }
           when nil # type 3 or 4 or 5 or 6, 7, 8
             if self.currency.in?(%w(CHF EUR)) # type 3, could be 5, 6, 7, 8
-              if self.service_level == 'SEPA' # type 5
-                # this is actually almost the international standard
-                return false if self.local_instrument.present? || self.payment_method.present? || self.creditor_agent.present? || self.reference_number.present?
-                self.currency == 'EUR'
-              elsif self.service_level.nil? # type 3, could be 6, 7, 8
+              if self.service_level.nil? # type 3, could be 6, 7, 8
                 raise 'types 3 and 6, 7, 8 not implemented'
-              else
+              else # doesn't match a type
                 false
               end
             else # type 4, could be 6, 8
